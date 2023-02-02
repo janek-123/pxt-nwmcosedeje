ad7d3f1b0f32b2acee84ad99106c9351c4af94d0
janek-123 <115696196+janek-123@users.noreply.github.com>
2023-02-02 11:35:33 +0100

    Hafo

--- LightLevel.ts
+++ LightLevel.ts
@@ -1,1 +1,39 @@
-function GetLightLevel() { input.lightLevel() };
+function GetLightLevel() { input.lightLevel(); }
+
+let minLightLevel : number; 
+
+let calibrating = false;
+
+function Calibrate(){
+    basic.clearScreen();
+    cCount = calibCount;
+
+    calibrating = true;
+}
+
+let vals : number[] = [];
+
+const calibCount = 50;
+let cCount : number;
+
+basic.forever(function() {
+    if (!calibrating) return;
+
+    vals.push(input.lightLevel());
+    basic.pause(100);
+
+    cCount -= 1;
+
+    if(cCount <= 0){
+        let total = 0;
+        for (let i = 0; i < vals.length; i++) total += vals[i];
+
+        let calibratedV = Math.round(total / vals.length);
+
+        minLightLevel = calibratedV > 30 ? calibratedV - 20 : 10;
+        
+        console.log(`MIN :${minLightLevel}`); 
+
+        calibrating = false;
+    }
+})
--- main.ts
+++ main.ts
@@ -3,39 +3,51 @@ radio.setFrequencyBand(7);
 radio.setTransmitSerialNumber(true);
 radio.setGroup(69);
 
-const lightMin = 50;
-
 let startTime = -1;
-let timeFinal;
+let timeFinal : number;
 
-input.onButtonPressed(Button.A, function(){
-    console.log(4545);
-}) 
+input.onButtonPressed(Button.A, function(){    
+    if (startTime == -1 || calibrating) return;
+
+    whaleysans.showNumber(Math.round(timeFinal / 1000));
+})
+
+input.onButtonPressed(Button.AB, function () {
+    Reset();
+})
+
+input.onButtonPressed(Button.B, function () {
+    Calibrate();
+})
 
 basic.forever(function() {
+    if (startTime == -1 || calibrating) return;
+    
+    TryStart();
+})
 
-    if (startTime == -1) return;
-
+function TryStart(){
     let cLight = input.lightLevel();
-
-
-    if (cLight < lightMin){
+    if (cLight < minLightLevel) {
         music.playTone(66, 5);
-        timeFinal = input.runningTime() - startTime;     
+        timeFinal = input.runningTime() - startTime;
 
         console.log(`TIME: ${timeFinal}`);
         startTime = -1;
     }
-})
+}
 
 radio.onReceivedNumber(function(receivedNumber: number) {
     if (startTime != -1) return;
 
-    if (receivedNumber == 1){
-        console.log("start");
-
-        radio.receivedPacket(RadioPacketProperty.Time);
-
-        startTime = input.runningTime();
-    }
+    if (receivedNumber == 1) StartTimer();
 })
+
+function StartTimer(){
+    console.log("start ");
+    startTime = input.runningTime();
+}
+
+function Reset(){
+    startTime = -1;
+}
