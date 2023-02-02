6652ab387b6dad6a437b9d2ca3ac3d85bec4cd3c
janek-123 <115696196+janek-123@users.noreply.github.com>
2023-02-02 10:46:37 +0100

    base

---
+++ LightLevel.ts
@@ -0,0 +1,1 @@
+function GetLightLevel() { input.lightLevel() };
--- main.ts
+++ main.ts
@@ -1,1 +1,38 @@
+radio.setTransmitPower(6);
+radio.setFrequencyBand(7);
+radio.setTransmitSerialNumber(true);
+radio.setGroup(69);
 
+const lightMin = 50;
+
+let startTime = -1;
+let timeFinal;
+
+
+basic.forever(function() {
+
+    if (startTime == -1) return;
+
+    let cLight = input.lightLevel();
+
+
+    if (cLight < lightMin){
+        music.playTone(66, 5);
+        timeFinal = input.runningTime() - startTime;     
+
+        console.log(`TIME: ${timeFinal}`);
+        startTime = -1;
+    }
+})
+
+radio.onReceivedNumber(function(receivedNumber: number) {
+    if (startTime != -1) return;
+
+    if (receivedNumber == 1){
+        console.log("start");
+
+        radio.receivedPacket
+
+        startTime = input.runningTime();
+    }
+})
